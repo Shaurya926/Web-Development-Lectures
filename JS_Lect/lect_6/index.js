a6c5bf0b5e691e35635736a28996fc48d6688bd3
Shaurya926 <santoshdubey7089@gmail.com>
2026-05-09 08:10:10 +0530

    WEB D - Set Interval Function

--- JS_Lect/lect_6/index.js
+++ JS_Lect/lect_6/index.js
@@ -1,25 +1,25 @@
-// let h1 = document.querySelector("h1")
-// let p = document.createElement("pee")
-// p.innerText = "hi"
-// h1.appendChild(p)
-// h1.remove()
-// console.log(h1);
+let h1 = document.querySelector("h1")
+let p = document.createElement("pee")
+p.innerText = "hi"
+h1.appendChild(p)
+h1.remove()
+console.log(h1);
 
-//console.log(h1.innerText);
-// h1.innerText="byee"
+console.log(h1.innerText);
+h1.innerText="byee"
 
-// let inp = document.querySelector("input")
-// let h1 = document.querySelector("h1")
-// let click = document.querySelector("button")
-// click.addEventListener("click", () => {
-//     h1.innerText+= inp.value
-//     inp.value=""
-// })
-// inp.addEventListener("input", (e) => {
-//      console.log(e.target.value);
-//      h1.innerText = e.target.value
+let inp = document.querySelector("input")
+let h1 = document.querySelector("h1")
+let click = document.querySelector("button")
+click.addEventListener("click", () => {
+    h1.innerText+= inp.value
+    inp.value=""
+})
+inp.addEventListener("input", (e) => {
+     console.log(e.target.value);
+     h1.innerText = e.target.value
     
-// })
+})
 
 
 setInterval(() => {
